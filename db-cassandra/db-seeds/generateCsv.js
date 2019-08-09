5ac9f8553ea6ce4b1c9bf2a25e0f6db1560ae493
A Easterwood <aliciaeasterwood@gmail.com>
2019-08-09 13:27:13 -0500

    modify seed

--- db-cassandra/db-seeds/generateCsv.js
+++ db-cassandra/db-seeds/generateCsv.js
@@ -2,43 +2,82 @@ const generateFakeData = require('./generateFakerData');
 const JSONToCSV = require('json2csv').parse;
 const FileSystem = require('fs');
 
-function generateUsers () {
-
+function generateOneMillionUsers () {
   let users = [];
 
-  for (let i = 1; i <= 10000000; i++) {
+  for (let i = 1; i <= 1000000; i++) {
     let userData = generateFakeData.User();
     users.push(userData);
   }
-
   const csv = JSONToCSV(users, { fields: ['name', 'email', 'default_address_zip'] })
   FileSystem.writeFileSync('./db-cassandra/db-csvFiles/userData.csv', csv)
 };
-generateUsers();
+generateOneMillionUsers();
 
-function generateVendors () {
+function appendOtherUsers () {
+  for (let append = 1; append <= 9; append++) {
+    let users = [];
 
+    for (let i = 1; i <= 1000000; i++) {
+      let userData = generateFakeData.User();
+      users.push(userData);
+    }
+    const csv = JSONToCSV(users, { fields: ['name', 'email', 'default_address_zip'] })
+    FileSystem.appendFileSync('./db-cassandra/db-csvFiles/userData.csv', csv)
+  }
+};
+appendOtherUsers();
+
+function generateOneMillionVendors () {
   let vendors = [];
 
-  for (let i = 1; i <= 10000000; i++) {
+  for (let i = 1; i <= 1000000; i++) {
     let vendorData = generateFakeData.Vendor();
     vendors.push(vendorData);
   }
-
   const csv = JSONToCSV(vendors, { fields: ['name', 'amz_holds_stock', 'free_returns', 'ships_on_saturday', 'ships_on_sunday', 'ships_from_zipcode', 'status'] })
   FileSystem.writeFileSync('./db-cassandra/db-csvFiles/vendorData.csv', csv)
 }
-generateVendors();
+generateOneMillionVendors();
 
-function generateItems () {
+function appendOtherVendors () {
+  for (let append = 1; append <= 9; append++) {
+    let vendors = [];
 
+    for (let i = 1; i <= 1000000; i++) {
+      let vendorData = generateFakeData.Vendor();
+      vendors.push(vendorData);
+    }
+
+    const csv = JSONToCSV(vendors, { fields: ['name', 'amz_holds_stock', 'free_returns', 'ships_on_saturday', 'ships_on_sunday', 'ships_from_zipcode', 'status'] })
+    FileSystem.appendFileSync('./db-cassandra/db-csvFiles/vendorData.csv', csv)
+  }
+};
+appendOtherVendors();
+
+function generateFiveHundredThousandItems () {
   let items = [];
 
-  for (let i = 1; i <= 10000000; i++) {
+  for (let i = 1; i <= 500000; i++) {
     let itemData = generateFakeData.Item();
     items.push(itemData);
   }
   const csv = JSONToCSV(items, { fields: ['item_id', 'name', 'vendor_id', 'items_condition', 'price', 'quantity_available', 'amz_holds_stock', 'free_returns', 'ship_from_zipcode'] })
   FileSystem.writeFileSync('./db-cassandra/db-csvFiles/itemData.csv', csv)
 }
-generateItems();
+generateFiveHundredThousandItems();
+
+function appendOtherItems () {
+  for (let append = 1; append <= 19; append++) {
+    let items = [];
+
+    for (let i = 1; i <= 500000; i++) {
+      let itemData = generateFakeData.Item();
+      items.push(itemData);
+    }
+    const csv = JSONToCSV(items, { fields: ['item_id', 'name', 'vendor_id', 'items_condition', 'price', 'quantity_available', 'amz_holds_stock', 'free_returns', 'ship_from_zipcode'] })
+    FileSystem.appendFileSync('./db-cassandra/db-csvFiles/itemData.csv', csv)
+  }
+};
+appendOtherItems();
+
